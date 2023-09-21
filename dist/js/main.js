5ff067c027e34fefcb176f3d1e854b9de489659d
Oleg <olegatortor111@gmail.com>
2023-09-21 22:58:24 +0300

    cr animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,121 +1,172 @@
-const hamburger = document.querySelector('.hamburger'),
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.close');
 
-hamburger.addEventListener('click' , () => {
-    menu.classList.add('active');
-});
-close.addEventListener('click' , () => {
-    menu.classList.remove('active');
-});
+    hamburger.addEventListener('click' , () => {
+        menu.classList.add('active');
+    });
+    close.addEventListener('click' , () => {
+        menu.classList.remove('active');
+    });
 
 
 
 
-function isElementInViewport(el) {
-    // let rect = el.getBoundingClientRect();
-    // const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    // const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+    function isElementInViewport(el) {
+        let rect = el.getBoundingClientRect();
+        return (
+        rect.bottom >= 0 &&
+        rect.right >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
 
-    // return (
-    //   rect.bottom >= 0 &&
-    //   rect.right >= 0 &&
-    //   rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-    //   rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-    // );
-    const rect = el.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    let element = document.getElementById('myElement');
 
-    // Перевірка, чи нижня межа елемента рівна нижній межі вікна
-    return rect.bottom <= windowHeight;
-}
+    let numDiagram = document.querySelectorAll('.centered-text'),
+        nameDiagram = document.querySelectorAll('.skills__name'),
+        itemDiagram = document.querySelectorAll('.skills__item'),
+        durationDiagram = 1500,
+        ctxElements = document.querySelectorAll(`.myChart`);
 
+    let proc = [
+        { name: 'Adobe Photoshop', value: 70 },
+        { name: 'Adobe Illustrator', value: 70 },
+        { name: 'Webflow', value: 55 },
+        { name: 'Figma', value: 85 },
+        { name: 'Canva', value: 80 },
+        { name: 'Gimp', value: 90 }
+    ];
 
+    function showDiagram() {
+        itemDiagram.forEach((element, i) => {
+            if (isElementInViewport(element) && !element.classList.contains('processed')) {
+                let diagramName = proc[i].name;
+                let diagramNum = proc[i].value;
 
-const proc = {
-    '1': '80',
-    '2': '20',
-    '3': '100',
-    '4': '60',
-    '5': '75',
-    '6': '80',
-}
-proc.forEach(diagram => {
-    console.log(diagram)
-});
+                nameDiagram[i].innerHTML = `${diagramName}`;
+                numDiagram[i].innerHTML = 0;
 
-proc.forEach(diagram => {
-    let percentageInput = 80;
-    let ctx = document.querySelectorAll('.myChart');
-    ctx.forEach(el => {
-        let myChart = new Chart(el.getContext('2d'), {
-            type: 'doughnut',
-            data: {
-                datasets: [{
-                    data: [percentageInput, 100 - percentageInput], 
-                    backgroundColor: ['#E577A2', '#FFAFE9'],
-                    hoverBackgroundColor: ['#E577A2', '#FFAFE9']
-                }]
-            },
-            options: {
-                cutoutPercentage: 0,
-                responsive: true,
-                maintainAspectRatio: false,
-                legend: {
-                    display: false 
-                },
-                tooltips: {
-                    enabled: false 
+                if (ctxElements[i]) {
+                    createDiagram(diagramNum, i, ctxElements[i]);
                 }
+                element.classList.add('processed');
+            } else if (element.classList.contains('processed') && !isElementInViewport(element)) {
+                element.classList.remove('processed');
             }
         });
-    });
-})
-
-
-
-
-
-
-
-let cat = document.querySelector(".about__cat"),
-    animEducation = document.querySelector(".about__education"),
-    animCerts = document.querySelector(".education__certs"),
-    animColumn = document.querySelector(".education__column");
-
-
-
-let shakeCat = anime({
-    targets: cat,
-    translateY: ['-30px', '30px'], // Зміщення вгору та вниз на 50px
-    duration: 1000, // Тривалість анімації
-    easing: 'easeInOutQuad',
-    direction: 'alternate', // Змінює напрямок анімації на кожному циклі
-    loop: true // Повторювати анімацію безкінечно
-});
-// Запускаємо анімацію
-shakeCat.play();
-
-function animСertificates() {
-    if(isElementInViewport(animEducation)) {
-        anime({
-            targets: animCerts,
-            translateX: [
-                { value: '-80%', duration: 0 }, // Початкове значення (без анімації)
-                { value: '0', duration: 1000, easing: 'easeInOutQuad' } // Зміна до 0 з анімацією
-            ]
-        }).play();
-        anime({
-            targets: animColumn,
-            delay: 800,
-            // duration: 1000,
-            translateX: [
-                { value: '-80%', duration: 0 }, // Початкове значення (без анімації)
-                { value: '0', duration: 300, easing: 'easeInOutQuad' } // Зміна до 0 з анімацією
-            ],
-            opacity: '1'
-        }).play();
-        window.removeEventListener("scroll", animСertificates);
     }
-}
-window.addEventListener("scroll", animСertificates);
+
+    function createDiagram(num, i, elem) {
+        if (elem) {
+            let arr = new Chart(elem.getContext('2d'), {
+                type: 'doughnut',
+                data: {
+                    datasets: [{
+                        data: [0, 100],
+                        backgroundColor: ['#E577A2', '#FFAFE9'],
+                        hoverBackgroundColor: ['#E577A2', '#FFAFE9'],
+                        borderWidth: 0
+                    }]
+                },
+                options: {
+                    cutoutPercentage: 0,
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    legend: {
+                        display: false
+                    },
+                    animation: {
+                        duration: durationDiagram,
+                    },
+                    tooltips: {
+                        enabled: false
+                    }
+                }
+            });
+
+            animNumber(arr, num, i);
+        }
+        
+    }
+
+    function animNumber(arr, num, i) {
+        anime({
+            targets: numDiagram[i],
+            innerHTML: `${num}%`,
+            round: 1,
+            duration: durationDiagram,
+            easing: 'linear',
+            update: function() {
+                arr.data.datasets[0].data = [num, 100 - num];
+                arr.update();
+            }
+        })
+    }
+    window.addEventListener('scroll', showDiagram);
+
+    showDiagram();
+
+
+
+
+    let cat = document.querySelector(".about__cat"),
+        animEducation = document.querySelector(".about__education"),
+        animCerts = document.querySelector(".education__certs"),
+        animColumn = document.querySelector(".education__column"),
+        animExperience = document.querySelector(".list__column_right");
+
+
+
+    let shakeCat = anime({
+        targets: cat,
+        translateY: ['-30px', '30px'], // Зміщення вгору та вниз на 50px
+        duration: 1000, // Тривалість анімації
+        easing: 'easeInOutQuad',
+        direction: 'alternate', // Змінює напрямок анімації на кожному циклі
+        loop: true // Повторювати анімацію безкінечно
+    });
+    // Запускаємо анімацію
+    shakeCat.play();
+
+    function animСertificates() {
+        if(isElementInViewport(animEducation)) {
+            anime({
+                targets: animCerts,
+                translateX: [
+                    { value: '-80%', duration: 0 }, // Початкове значення (без анімації)
+                    { value: '0', duration: 900, easing: 'easeInOutQuad' } // Зміна до 0 з анімацією
+                ]
+            }).play();
+            anime({
+                targets: animColumn,
+                delay: 700,
+                // duration: 1000,
+                translateX: [
+                    { value: '-80%', duration: 0 }, // Початкове значення (без анімації)
+                    { value: '0', duration: 300, easing: 'easeInOutQuad' } // Зміна до 0 з анімацією
+                ],
+                opacity: '1'
+            }).play();
+            window.removeEventListener("scroll", animСertificates);
+        }
+    }
+
+    function animExperiences() {
+        if(isElementInViewport(animExperience)) {
+            anime({
+                targets: animExperience,
+                translateX: [
+                    { value: '80%', duration: 0 }, // Початкове значення (без анімації)
+                    { value: '0', duration: 900, easing: 'easeInOutQuad' } // Зміна до 0 з анімацією
+                ]
+            }).play();
+            window.removeEventListener("scroll", animExperiences);
+        }
+    }
+
+    window.addEventListener("scroll", animСertificates);
+    window.addEventListener("scroll", animExperiences);
+})
